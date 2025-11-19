9beb74555d38953363942dd72aa605040dc2f61e
maaz375 <maaz.mujeeb.00000@gmail.com>
2025-11-19 17:49:49 +0500

    added class2 of js

---
+++ javascript/class.2/app.js
@@ -0,0 +1,29 @@
+// var myName = camel case
+// var MyName = pascal case
+// myname kabab case
+
+var name1 = 'saylani';
+var num1 = 12;
+var num2 = 10;
+var num3 = '11';
+var num4 = '11';
+var greeting = 'assalamualaikum'
+
+// document.write(name1 + '<br>')
+// document.write(num1)
+
+// document.write(greeting + ' ' + 'Maaz' )
+
+// document.write(num1 +  num2 + '<br>' )
+// document.write(num3 + num4 + '<br>' )
+// document.write(num3 + num1 + '<br>')
+// document.write(num3 - num1 + '<br>')
+// document.write(num1 * num3 + '<br>')
+// document.write(num1 / num4 + '<br>')
+// document.write(num3 * num4  + '<br>')
+
+var originalNum = "23";
+document.write(originalNum)
+
+var newNum = originalNum + 7;
+document.write(newNum)
