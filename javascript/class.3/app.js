07211c7ffafa86f9fc7724c0899d87038484081f
maaz375 <maaz.mujeeb.00000@gmail.com>
2025-11-21 17:49:39 +0500

    added class 3 of js

---
+++ javascript/class.3/app.js
@@ -0,0 +1,30 @@
+var firstName = 'new';
+var lastName = 'world';
+var num1 = '1';
+var num2 = '2';
+
+// document.write( firstName + ' ' + lastName );
+// console.log(firstName+lastName)
+// console.log(num1)
+// console.log(num1 - num2)
+
+// var student1 = 19;
+// var student2 = 15;
+// var newTotal = student1 + student2;
+
+// document.write(newTotal)
+
+// var num1 = 5 % 2;
+// document.write(num1)
+
+// var num1 = 10;
+
+// num1++;
+// num1--;
+
+// document.write(num1);
+
+var num = 1;
+var newNum = ++num;
+
+document.write(num);
