f4516cb0d63bb5933a24ed9f2513e4d15f8c0ace
maaz375 <maaz.mujeeb.00000@gmail.com>
2025-11-24 17:49:00 +0500

    added class 4 of js

---
+++ javascript/class.4/app.js
@@ -0,0 +1,37 @@
+// var num1 = 10;
+// console.log(num1++)
+
+// console.log(num1)
+
+// var num2 = 20;
+// console.log(++num2)
+
+// var totalCost1 = 1 + (3 * 4);
+// var totalCost2 = (1 + 3) * 4;
+
+// document.write('totalCost1 :' ,totalCost1 + '<br>')
+// document.write('totalCost2 :' ,totalCost2)
+
+// var name = prompt('enter Your Name')
+// var phone = prompt('your number')
+
+// console.log(name)
+
+// var num1 = prompt('enter first number')
+// var num2 = prompt('enter second number')
+
+// document.write(Number(num1) + Number(num2))
+
+// var x = prompt("Where does the Pope live?");
+// if (x === "Vatican") {
+//     alert("Correct!");
+// }
+
+
+var x = prompt("Where does the Pope live?")
+if(x === "Vatican"){
+    alert("Correct Answer!")
+}else{
+    alert("Tu thora sa ladu h kia ?")
+}
+console.log(x)
