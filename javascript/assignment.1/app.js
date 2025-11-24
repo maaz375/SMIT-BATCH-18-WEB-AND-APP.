3d2e8b2db06bd2b15cfffe7d75b26f921d7ffbd5
maaz375 <maaz.mujeeb.00000@gmail.com>
2025-11-25 00:08:14 +0500

    added assignment 1 of js

---
+++ javascript/assignment.1/app.js
@@ -0,0 +1,45 @@
+var username;
+
+var myName = 'Mujeeb Ahmed';
+
+var message = 'Hello World';
+
+alert(message);
+
+var studentName = 'Mujeeb Ahmed';
+var studentAge = '14 Year Old';
+var studentBatch = ' Batch 18';
+var studentCourse = 'Modern Web N App Development';
+var studentSirName = 'Sir Majid Hussain';
+
+alert(studentName);
+alert(studentAge);
+alert(studentBatch);
+alert(studentCourse);
+alert(studentSirName);
+
+var food = "Pulao \n Pula \n Pul \n Pu \n P";
+
+alert(food)
+
+var numberOfChildren = 1;
+var partnerName = "Maaz";
+var location = "Germany";
+var job = "Web Developer";
+
+var fortune = "You will be a " + job + " in " + location + " married to " + partnerName + " with " + numberOfChildren + " children.";
+
+alert(fortune);
+
+var emailName = 'My Email Address is';
+var email = 'maaz.mujeeb.00000@gmail.com';
+
+alert(emailName + ' ' + email)
+
+var book = 'I am trying learn from the book A smarter way to learn JavaScript';
+
+alert(book)
+
+var text = 'Yay! I can  write HTML content through Javascript'
+
+document.write(text)
