5d57cfce1968a179e05b7b2096a0ad25bf7d0474
maaz375 <maaz.mujeeb.00000@gmail.com>
2025-11-26 00:24:01 +0500

    added ass 2 of js

---
+++ javascript/assignment.2/app.js
@@ -0,0 +1,19 @@
+var age = 'I am 15 year old';
+
+alert(age)
+
+var visit = '“You have visited this site 1 times';
+
+alert(visit)
+
+var birthYear = 'My birth yaer is 2010';
+
+document.write(birthYear + '<br>')
+
+var visitorName = "John Doe";
+var productTitle = "T-shirt";
+var quantity = 5;
+
+document.write(
+    visitorName + " ordered " + quantity + " " + productTitle +" on XYZ Clothing store."
+)
