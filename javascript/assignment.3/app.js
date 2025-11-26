629b99de1ce73619f84227a7114eda38fd8587ef
maaz375 <maaz.mujeeb.00000@gmail.com>
2025-11-26 17:56:48 +0500

    added class 5 of js

---
+++ javascript/assignment.3/app.js
@@ -0,0 +1,28 @@
+var a = 5, b = "hellow", c = world;
+
+// legal variables
+var myAge;
+var $Doller;
+var _game;
+var user1;
+var full_name;
+
+// illegal variables
+
+// sir mene ye illegal variables ko commit kia h lekn phr bhi show nhi ho rha h ye koi error nhi aarha h 
+
+// var 1user;
+// var my-name;
+// var var ;
+// var user name;
+// var @data;
+
+document.write("Rules for naming JS variables" + '<br>');
+
+document.write("Variable names can only contain letters, numbers, $ and _. For example: $my_1stVariable" + '<br>' + '<br>');
+
+document.write("Variables must begin with a letter, $ or _. For example: $name, _name or name" + '<br>' + '<br>');
+
+document.write("Variable names are case sensitive" + '<br>' + '<br>');
+
+document.write("Variable names should not be JS keywords");
---
+++ javascript/class.5/app.js
@@ -0,0 +1,21 @@
+// let isHungry = true;
+
+// if(isHungry) {
+//     console.log("let's eat!")
+// } else {
+//     console.log("Maybe later...")
+// }
+
+// Primitive data types
+// 1. string
+// 2. number
+
+// Non primitive data types
+
+var myName = undefined;
+var myName = null;
+let name1 = undefined;
+name1 = '';
+var name3 = 0;
+const war = 1234;
+war = 12;
