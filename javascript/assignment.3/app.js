a66195ca3827650e0fc6eb6a1239bd6acdb66f01
maaz375 <maaz.mujeeb.00000@gmail.com>
2025-11-27 00:15:58 +0500

    changes in ass 3 of js

--- javascript/assignment.3/app.js
+++ javascript/assignment.3/app.js
@@ -1,4 +1,4 @@
-var a = 5, b = "hellow", c = world;
+var a = 5, b = "hellow", c = "world";
 
 // legal variables
 var myAge;
@@ -9,20 +9,21 @@ var full_name;
 
 // illegal variables
 
-// sir mene ye illegal variables ko commit kia h lekn phr bhi show nhi ho rha h ye koi error nhi aarha h 
-
 // var 1user;
 // var my-name;
 // var var ;
 // var user name;
 // var @data;
 
-document.write("Rules for naming JS variables" + '<br>');
+document.write("<h2> <center> Rules for naming JS variables </center> </h2> <br>")
 
-document.write("Variable names can only contain letters, numbers, $ and _. For example: $my_1stVariable" + '<br>' + '<br>');
+document.write("Variable names can only contain letters, numbers, $ and _. <br>");
 
-document.write("Variables must begin with a letter, $ or _. For example: $name, _name or name" + '<br>' + '<br>');
+document.write("For example: $my_1stVariable <br><br>");
 
-document.write("Variable names are case sensitive" + '<br>' + '<br>');
+document.write("Variables must begin with a letter, $ or _. <br>");
+document.write("For example: $name, _name or name <br><br>");
 
-document.write("Variable names should not be JS keywords");
+document.write("Variable names are case sensitive. <br><br>");
+
+document.write("Variable names should not be JS reserved words. <br>");
---
+++ javascript/assignment.4/app.js
@@ -0,0 +1,15 @@
+var num1 = 13;
+var num2 = 9;
+var sum = num1 + num2;
+
+document.write("The sum of 13 n 9 is " + sum + '<br>')
+
+var subtract = num1 - num2;
+
+document.write("Result of subtracting 9 from 13 is " + subtract + '<br>')
+
+var multiply = num1 * num2;
+
+document.write("Result of multiplying 13 n 9 is " + multiply)
+
+var divide = num1
