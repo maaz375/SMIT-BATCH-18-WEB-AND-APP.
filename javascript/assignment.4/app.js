82394b9618d0510351f9a0649772cda7607e54ca
maaz375 <maaz.mujeeb.00000@gmail.com>
2025-11-28 17:49:33 +0500

    added class 15 of css n some changes in ass 4 of js n class 5 of js

--- javascript/assignment.4/app.js
+++ javascript/assignment.4/app.js
@@ -2,14 +2,148 @@ var num1 = 13;
 var num2 = 9;
 var sum = num1 + num2;
 
-document.write("The sum of 13 n 9 is " + sum + '<br>')
+alert("The sum of 13 n 9 is " + sum)
 
 var subtract = num1 - num2;
 
-document.write("Result of subtracting 9 from 13 is " + subtract + '<br>')
+alert("Result of subtracting 9 from 13 is " + subtract)
 
 var multiply = num1 * num2;
 
-document.write("Result of multiplying 13 n 9 is " + multiply)
+alert("Result of multiplying 13 n 9 is " + multiply)
 
-var divide = num1
+var divide = num1 / num2
+
+alert("Result of dividing 13 by 9 is " + divide)
+
+var remainder = num1 % num2
+
+alert("Remainder of dividing 13 by 9 is " + remainder)
+
+
+
+var num = 3;
+
+document.write("Value after variable declaration is " + num + '<br>' + '<br>')
+
+document.write("Initial value: " + num + '<br>' + '<br>')
+
+num++;
+
+document.write("Value after increment is: " + num + '<br>' + '<br>')
+
+num = num + 7;
+
+document.write("Value after addition is: " + num + '<br>' + '<br>')
+
+num--;
+
+document.write("Value after decriment is: " + num + '<br>' + '<br>')
+
+num = num % 3;
+
+document.write("The remainder is: " + num + '<br>' + '<br>' + '<br>' + '<br>')
+
+
+
+var ticket = 600;
+var totalCoast = ticket * 5;
+
+document.write("Total cost to buy 5 tickets to a movie is: " + totalCoast + "PKR" + '<br>' + '<br>' + '<br>' + '<br>')
+
+
+
+var num8 = 8;
+
+document.write("Multiplication table of " + num8 + '<br>' + '<br>')
+
+for (var i = 1;
+    i <= 10;
+    i++) {
+    document.write(num8 + " x " + i + " = " + (num8 * i) + '<br>');
+}
+
+document.write('<br>' + '<br>')
+
+
+
+var c = 12;
+var f = (c * 9 / 5) + 32;
+document.write(c + "°C is " + f + "°F" + '<br>');
+
+f = 68;
+c = (f - 32) * 5 / 9;
+document.write(f + "°F is " + c + "°C" + '<br>');
+
+
+
+var priceItem1 = 560;
+var priceItem2 = 200;
+
+var quantityItem1 = 2;
+var quantityItem2 = 5;
+
+var shippingCharges = 150;
+
+var totalCost = (priceItem1 * quantityItem1) + (priceItem2 * quantityItem2) + shippingCharges;
+
+document.write("<h2> Shopping Cart </h2>");
+document.write("Price of item 1 is: " + priceItem1 + "PKR <br>");
+document.write("Quantity of item 1 is: " + quantityItem1 + '<br>');
+document.write("Price of item 2 is: " + priceItem2 + "PKR<br>");
+document.write("Quantity of item 2 is: " + quantityItem2 + '<br> <br>');
+document.write("Shipping Charges: " + shippingCharges + "PKR <br> <br>");
+document.write("<b>Total Cost of your order is: " + totalCost + " PKR </b> <br> <br> <br>");
+
+
+
+var totalMarks = 1200;
+var marksObtained = 900;
+
+var percentage = (marksObtained / totalMarks) * 100;
+
+document.write("<h2> Marks Sheet </h2>");
+document.write("Total Marks: " + totalMarks + '<br>');
+document.write("Marks Obtained: " + marksObtained + '<br>');
+document.write("Percentage: " + percentage + "% <br> <br> <br>");
+
+
+
+var totalPKR = (10 * 104) + (25 * 28);
+
+document.write("<h2> Currency in PKR </h2>")
+document.write("Total Currency in Pakistani Rupees: " + totalPKR + " PKR <br> <br> <br> <br>");
+
+
+
+var num = 5;
+
+var result = ((num + 5) * 10) / 2;
+
+alert(result);
+
+
+
+var currentYear = 2025;
+var birthYear = 2010;
+
+var age1 = currentYear - birthYear;
+var age2 = age1 - 1;
+
+document.write("<b> The Age Calculator </b> <br> <br>")
+document.write("Current Year: " + currentYear +'<br>')
+document.write("Birth Year: " + birthYear + '<br> <br>')
+document.write("They are either " + age2 + " or " + age1 + " years old. <br> <br> <br> <br>");
+
+
+
+var radius = 12;
+var pi = 3.142;
+
+var circumference = 2 * pi * radius;
+var area = pi * radius * radius;
+
+document.write("<h2> <b> The Geometrizer </b> </h2>");
+document.write("Radius of a circle: " + radius + '<br>');
+document.write("The circumference is: " + circumference + '<br>');
+document.write("The area is: " + area);
--- javascript/class.5/app.js
+++ javascript/class.5/app.js
@@ -9,6 +9,9 @@
 // Primitive data types
 // 1. string
 // 2. number
+// 3. undefined
+// 4. null
+// 5. boolean = true / false
 
 // Non primitive data types
 
@@ -17,5 +20,18 @@ var myName = null;
 let name1 = undefined;
 name1 = '';
 var name3 = 0;
-const war = 1234;
-war = 12;
+const war = 'america';
+var war2 = 12;
+// let myWorld;
+// myWorld = 'it is'
+
+// if(war == 'america'){
+
+// }
+
+if (war2 == '12') {
+    alert('yes it is')
+}
+else {
+    alert('not')
+}
