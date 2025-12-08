11e1cd98476dd6d01ffefdef57c929fcab45e70d
maaz375 <maaz.mujeeb.00000@gmail.com>
2025-12-08 17:50:23 +0500

    added classes of js

---
+++ javascript/calculator.7/app.js
@@ -0,0 +1,18 @@
+var num1;
+var num2;
+
+var sum = num1 + num2;
+var sub = num1 - num2;
+var multiply = num1 * num2;
+var division = num1 / num2;
+
+var answer;
+
+var take = Number(prompt('what wanna you do like ( + , - , * , / )'))
+alert = Number(prompt('Enter 1 Number'))
+alert = Number(prompt('Enter 2 Number'))
+
+if( take == - true ){
+    answer = sub
+    document.write(` Your answer is ${answer} `)
+}
---
+++ javascript/class.9/app.js
@@ -0,0 +1,34 @@
+// var weight = 1000;
+// var time = 10;
+
+// if (weight > 700 && time > 12) {
+// document.write(`<h2> Yeah it's right </h2>`);
+// } else {
+//     document.write(`No it's wrong`);
+// }
+
+// if (weight > 700 || time > 12) {
+// document.write(`<h2> Yeah it's right </h2>`);
+// } else {
+//     document.write(`No it's wrong`);
+// }
+
+// var str = '';
+// document.write(typeof str);
+
+// var isAvailable = true;
+// document.write(typeof isAvailable);
+
+// non-primitive;
+// 1.Array
+// 2.Object
+
+// var myData = [];
+// document.write(Array.isArray(myData))
+
+var arr1 = [12, 13, 15, 20, "yellow", 'saylani', true, undefined, null];
+// document.write(arr1[5])
+
+var index2 = 100;
+arr1[2] = index2
+document.write(arr1);
