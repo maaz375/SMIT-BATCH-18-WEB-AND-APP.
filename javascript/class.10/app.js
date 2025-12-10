f0eab5059915a5fb0905b6fafdb526f57f4d232f
maaz375 <maaz.mujeeb.00000@gmail.com>
2025-12-10 17:52:32 +0500

    added class 10 of js

---
+++ javascript/class.10/app.js
@@ -0,0 +1,31 @@
+// var fruits = ['pomogrenate', 'kiwi', 'banana'];
+
+// console.log('before pop',fruits);
+
+// fruits.pop();
+
+// console.log('after pop', fruits);
+
+// fruits.push('orange', 'grapes', 'mamufruits');
+
+// console.log('push', fruits);
+
+
+
+// var fruits = ['pomogrenate', 'kiwi', 'banana'];
+
+// fruits.shift();
+
+// console.log('after shift', fruits);
+
+// fruits.unshift('blueberry', 'avocado', 'dragonfriut');
+
+// console.log('unshift', fruits);
+
+
+
+// var fruits = ['pomogrenate', 'kiwi', 'banana'];
+
+// fruits.splice(1,1,'lichi', 'ami', 'baba');
+
+// console.log('after', fruits)
