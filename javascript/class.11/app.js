06bab66482bec639d2156cd9a9bcf6531eb97b27
maaz375 <maaz.mujeeb.00000@gmail.com>
2025-12-13 19:53:19 +0500

    add online class code

--- javascript/class.11/app.js
+++ javascript/class.11/app.js
@@ -23,18 +23,57 @@
 //  FUNCTION: CH.35
 
 
-function alertUser (name, greet) {
-    // var userName = prompt('user name');
-    alert(`As salamu alaikum ${name}`);
-    alert(` ${greet}`)
-}
+// function alertUser (name, greet) {
+//     // var userName = prompt('user name');
+//     alert(`As salamu alaikum ${name}`);
+//     alert(` ${greet}`)
+// }
 
 // document.write(`Hellow <br>`);
 // document.write(`Where r u <br>`);
 // document.write(`Tera dihaan kidher h <br>`);
 // document.write(`Chal by nikal yaha sy warna pity gha <br>`);
 
-var name1 = 'Saylani';
-var halChal = 'How r u';
+// var name1 = 'Saylani';
+// var halChal = 'How r u';
 
-alertUser(name1, halChal);
+// alertUser(name1, halChal);
+
+
+
+//  function online class
+
+
+
+function Calculate(n1, n2, operator){
+    var total;
+    if(operator === '+'){
+        total = n1 + n2;
+    } else if (operator === '-'){
+        total = n1 - n2;
+    } else if (operator === '*'){
+        total = n1 * n2;
+    } else if (operator === '/'){
+        total = n1 / n2;
+    } else {
+        alert('please enter right number or operator');
+    }
+
+    return total;
+}
+
+
+var num1 = Number(prompt('enter num1'));
+var num2 = Number(prompt('enter num2'));
+var operator = prompt('+, -, *, /');
+
+var output = Calculate(num1, num2, operator);
+
+// console.log('output', output);
+
+
+var heading = document.getElementById('h2');
+heading.innerHTML = `Calculation is ${output}`;
+// heading.innerText = 'now you can do';
+
+// console.log('heading', heading);
