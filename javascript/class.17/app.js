a6e7a4e16cd1fcb30a1503cf1b647efe9c806913
maaz375 <maaz.mujeeb.00000@gmail.com>
2025-12-26 17:55:49 +0500

    string methods

---
+++ javascript/class.17/app.js
@@ -0,0 +1,69 @@
+//  Finding string 
+
+
+// var str1 = "The name";
+
+// for (var i = 0; i < str1.length; i++) {
+//     console.log(str1[i] + "<br>");
+// };
+
+
+// var arr1 = ["BMW", "Saab", "Volvo", "Audi", "Chevrolet", "GWM", "Toyota", "KIA", "hyundai"];
+
+// console.log(arr1.indexOf("BMW"));
+
+// console.log(arr1.includes("Volvo"));
+
+
+// var str1 = "The last name of the person is Khan";
+
+// var finding = str1.indexOf("K");
+
+// console.log(finding);
+
+
+// var arr1 = ["BMW", "Saab", "Volvo", "Audi", "Chevrolet", "GWM", "Toyota", "KIA", "hyundai"];
+
+// console.log(arr1.indexOf("shehbaz"));
+
+
+
+// var text = "just imagine World War II";
+
+// for (var i = 0; i < text.length; i++) {
+//     if (text.slice(i, i + 12) === "just imagine") {
+//         text = text.slice(0, i) + "Don't imagine" + text.slice(i + 12);
+//     };
+// };
+
+// console.log(text);
+
+
+// var text = "just imagine World War II";
+
+// var newText = text.replace("just imagine", "Don't imagine");
+
+// console.log(newText);
+
+
+
+// var str = 'his car is red also his bike is red also his house is red';
+
+// var change = str.replace(/red/g, 'blue');
+// var change = str.replaceAll('red', 'blue');
+
+// console.log(change);
+
+
+
+// var arr1 = ["BMW", "Saab", "Volvo", "Audi", "Chevrolet", "GWM", "Toyota", "KIA", "hyundai"];
+
+// var chan = arr1.charAt(1);
+
+// console.log(chan);
+
+
+
+var str = 'his car is red also his bike is red also his house is red';
+
+console.log(str.charAt(1));
